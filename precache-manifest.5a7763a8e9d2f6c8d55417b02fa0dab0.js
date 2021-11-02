378fbc3a3b8dcbf4c2d91e10f6b43acc1227a871
James Young <james.young@ed.ac.uk>
2021-11-02 11:03:23 +0000

    Updates

---
+++ precache-manifest.5a7763a8e9d2f6c8d55417b02fa0dab0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e10f22223e3e26dfaac2f9350f6ff8f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c4451b55627bb6b5220f",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/home/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4451b55627bb6b5220f",
+    "url": "/home/static/js/main.4126421c.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "ff3d96621fa0279715caa4f8618aaf53",
+    "url": "/home/static/media/jamesyoung.ff3d9662.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.5a7763a8e9d2f6c8d55417b02fa0dab0.js"
 );
 
 self.addEventListener('message', (event) => {
